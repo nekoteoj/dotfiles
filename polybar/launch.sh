922423ecc2a43d287f15b7b2a894429fd26ae105
Pisit Wajanasara <compisit1234@gmail.com>
2022-09-08 03:59:58 +0700

    Customized config for Asus Zenbook

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,4 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-polybar nekoteoj_1 -r &
+# polybar nekoteoj_1 -r &
+
+# Check for each display
+my_laptop_internal_monitor=$(xrandr --query | grep 'eDP-1')
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-2')
+if [[ $my_laptop_internal_monitor = *connected* ]]; then
+    polybar nekoteoj_1 -r &
+fi
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar nekoteoj_2 -r &
+fi
